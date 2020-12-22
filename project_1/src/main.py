cfff00c7b51a79d8d2e034d2f8634c06af9e3aa5
drenfr01 <duncanrenfrow@gmail.com>
2020-12-21 19:49:09 -0500

    adding skeleton of neural network

--- project_1/src/main.py
+++ project_1/src/main.py
@@ -1,6 +1,5 @@
-from my_xgboost import MyXGB
 from helper_methods import LoadDataset
-from data_munging_methods import  DataMunge
+from data_munging_methods import DataMunge
 
 
 def define_features() -> tuple[list, list, list]:
@@ -41,8 +40,8 @@ def define_features() -> tuple[list, list, list]:
                     'WarrantyCost'
                     ]
 
+    return cat_features, num_features, drop_features
 
-    return (cat_features, num_features, drop_features)
 
 if __name__ == '__main__':
     dataset = "data/DontGetKicked/training.csv"
@@ -52,11 +51,8 @@ if __name__ == '__main__':
     load_dataset = LoadDataset(dataset, "IsBadBuy", "RefId", date_cols=date_features)
     X, y = load_dataset.split_target()
 
-
     datamunge = DataMunge()
 
     X = datamunge.transform_variables(X, cat_features, num_features, date_features, drop_features)
 
     X_train, X_valid, X_test, y_train, y_valid, y_test = load_dataset.partition(X, y)
-
-
--- project_1/src/my_nn.py
+++ project_1/src/my_nn.py
@@ -1,6 +1,71 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import torch
+import pandas as pd
+import torch.utils.data as data_utils
 
-class MyNN:
-    def __init__(self, random_state):
-        self.random_state = random_state
 
+class Net(nn.Module):
+    def __init__(self, input_size):
+        super(Net, self).__init__()
+
+
+class MyNet():
+    """Wrapper class for neural network
+
+    """
+    def __init__(self, input_size: int, num_epochs: int, batch_size: int,
+                 X: pd.DataFrame, y: pd.Series):
+        self.input_size = input_size
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.X = X
+        self.y = y
+
+    # Code taken from https://stackoverflow.com/questions/50307707/convert-pandas-dataframe-to-pytorch-tensor
+    # Anh-Thi DINH
+    @staticmethod
+    def get_device():
+        if torch.cuda.is_available():
+            device = torch.device('cuda:0')
+        else:
+            device = torch.device('cpu')
+        return device
+
+    # Code taken from https://stackoverflow.com/questions/50307707/convert-pandas-dataframe-to-pytorch-tensor
+    # Gaurav Shrivastava
+    def df_to_trainloader(self) -> data_utils.DataLoader:
+        # see https://stackoverflow.com/questions/13187778/convert-pandas-dataframe-to-numpy-array
+        train = data_utils.TensorDataset(torch.Tensor(self.X.to_numpy()), torch.Tensor(self.y.to_numpy()))
+        train_data = data_utils.DataLoader(train, batch_size = self.batch_size, shuffle=True)
+        return train_data
+
+    # see PyTorch tutorial
+    def train_nn(self) -> None:
+        net = Net(self.input_size)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.09)
+
+        device = self.get_device()
+        data_loader = self.df_to_trainloader()
+
+        for epoch in range(self.num_epochs):
+
+            running_loss = 0.0
+            for i, data in enumerate(data_loader, 0):
+                inputs, labels = data
+
+                optimizer.zero_grad()
+
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                if i % 2000 == 1999: # print every 2000 mini-batches
+                    print(f"[{epoch +1} {i+1}] loss: {running_loss / 2000}")
+                    running_loss = 0.0
+
+        return nn
