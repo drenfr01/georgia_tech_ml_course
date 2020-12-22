6da9c0dd9c21bebad92c7b40a095066c609de1db
drenfr01 <duncanrenfrow@gmail.com>
2020-12-21 19:58:25 -0500

    partway through defining nn architecture

--- project_1/src/my_nn.py
+++ project_1/src/my_nn.py
@@ -9,6 +9,14 @@ import torch.utils.data as data_utils
 class Net(nn.Module):
     def __init__(self, input_size):
         super(Net, self).__init__()
+        # start with 1 hidden layer of 64 units
+        self.fc1 = nn.Linear(self.input_size, 64)
+        self.layer_out = nn.Linear(64, 1)
+
+    def forward(self, x):
+        # TODO: explore dropout
+        x = F.relu(self.fc1(x))
+        x = F.sigmoid(self.layer_out(x))
 
 
 class MyNet():
@@ -16,12 +24,13 @@ class MyNet():
 
     """
     def __init__(self, input_size: int, num_epochs: int, batch_size: int,
-                 X: pd.DataFrame, y: pd.Series):
+                 X: pd.DataFrame, y: pd.Series, save_path: str):
         self.input_size = input_size
         self.num_epochs = num_epochs
         self.batch_size = batch_size
         self.X = X
         self.y = y
+        self.save_path = save_path
 
     # Code taken from https://stackoverflow.com/questions/50307707/convert-pandas-dataframe-to-pytorch-tensor
     # Anh-Thi DINH
@@ -40,6 +49,7 @@ class MyNet():
         train = data_utils.TensorDataset(torch.Tensor(self.X.to_numpy()), torch.Tensor(self.y.to_numpy()))
         train_data = data_utils.DataLoader(train, batch_size = self.batch_size, shuffle=True)
         return train_data
+
 
     # see PyTorch tutorial
     def train_nn(self) -> None:
